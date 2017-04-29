e8437318e37832d3e874057c443d57f0325683d2
Nisha Salim <nocturne.noire@gmail.com>
2017-04-29 23:31:27 +0530

    Add tests to check version'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
-  namespace :api, defaults: { format: :json } do
-    # resources
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # resources
+    end
   end
 end
